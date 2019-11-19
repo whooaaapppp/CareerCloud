3cb19f5eee4da9d927fe7da660cdba95dc354d2d
whooaaapppp <jwarain@me.com>
2019-11-19 00:21:51 -0500

    added ApplicaitonJobApplicationPoco

---
+++ CareerCloud.Pocos/ApplicantJobApplicationPoco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Applicant_Job_Applications")]
+    class ApplicantJobApplicationPoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid Applicant { get; set; }
+        public Guid Job { get; set; }
+
+        [Column("Application_Date")]
+        public DateTime ApplicationDate { get; set; }
+        [Column("Time_Stamp")]
+        public Byte[] TimeStamp { get; set; }
+
+    }
+}
