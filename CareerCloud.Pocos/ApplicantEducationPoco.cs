19dd321f61df76f8a1567fd69b9b25283ada975b
whooaaapppp <jwarain@me.com>
2019-11-18 23:59:18 -0500

    updated with CC Unit Test Assignment 1

--- CareerCloud.Pocos/ApplicantEducationPoco.cs
+++ CareerCloud.Pocos/ApplicantEducationPoco.cs
@@ -5,20 +5,25 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace CareerCloud.Pocos
 {
     [Table ("Applicant_Educations")]
-    public class ApplicantEducationPoco
+    public class ApplicantEducationPoco : IPoco
     {   
         [Key] 
         public Guid Id { get; set; }
         public Guid Applicant { get; set; }
         public string Major { get; set; }
+
         [Column("Certificate_Diploma")]
         public string  CertificateDiploma { get; set; }
+
         [Column("Start_Date")]
-        public DateTime StartDate { get; set; }
+        public DateTime? StartDate { get; set; }
+
         [Column("Completion_Date")]
-        public DateTime CompletionDate { get; set; }
-        [Column("Completion_Percernt")]
-        public Byte CompletionPercent { get; set; }
+        public DateTime? CompletionDate { get; set; }
+
+        [Column("Completion_Percent")]
+        public Byte? CompletionPercent { get; set; }
+
         [Column("Time_Stamp")]
         public Byte[] TimeStamp { get; set; }
     }
