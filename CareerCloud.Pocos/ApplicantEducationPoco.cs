200f4b44aeda3642eaf5306beea2633eb7095f0d
whooaaapppp <jwarain@me.com>
2019-11-18 23:12:23 -0500

    adding annotation

--- CareerCloud.Pocos/ApplicantEducationPoco.cs
+++ CareerCloud.Pocos/ApplicantEducationPoco.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CareerCloud.Pocos
 {
+    [Table ("Applicant_Educations")]
     public class ApplicantEducationPoco
-    {
+    {   
+        [Key] 
         public Guid Id { get; set; }
         public Guid Applicant { get; set; }
         public string Major { get; set; }
+        [Column("Certificate_Diploma")]
         public string  CertificateDiploma { get; set; }
+        [Column("Start_Date")]
         public DateTime StartDate { get; set; }
+        [Column("Completion_Date")]
         public DateTime CompletionDate { get; set; }
+        [Column("Completion_Percernt")]
         public Byte CompletionPercent { get; set; }
+        [Column("Time_Stamp")]
         public Byte[] TimeStamp { get; set; }
     }
 }
