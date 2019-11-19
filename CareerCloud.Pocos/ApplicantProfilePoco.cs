17129ac9ed7806b002586783e8e1e1163fb7f11b
whooaaapppp <jwarain@me.com>
2019-11-19 00:41:00 -0500

    added ApplicantProfile Poco

---
+++ CareerCloud.Pocos/ApplicantProfilePoco.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Applicant_Profiles")]
+    public class ApplicantProfilePoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid Login { get; set; }
+        public string Currency { get; set; }
+
+        [Column("Current_Salary")]
+        public Decimal? CurrentSalary { get; set; }
+        [Column("Current_Rate")]
+        public Decimal? CurrentRate { get; set; }
+        [Column("Country_Code")]
+        public string Country { get; set; }
+        [Column("State_Province_Code")]
+        public string Province { get; set; }
+        [Column("Street_Address")]
+        public string Street { get; set; }
+        [Column("City_Town")]
+        public string City { get; set; }
+        [Column("Zip_Postal_Code")]
+        public string PostalCode { get; set; }
+        [Column("Time_Stamp")]
+        public Byte[] TimeStamp { get; set; }
+
+    }
+}
