bde33f59c21587b6b159bf874426466ee0989c6e
whooaaapppp <jwarain@me.com>
2019-11-19 19:22:32 -0500

    adding applicantworkhistory and companydescription poco

---
+++ CareerCloud.Pocos/CompanyDescriptionPoco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{   [Table("Company_Descriptions")]
+    public class CompanyDescriptionPoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid Company { get; set; }
+        public string LanguageId { get; set; }
+
+        [Column("Company_Name")]
+        public string CompanyName { get; set; }
+        [Column("Company_Description")]
+        public string CompanyDescription { get; set; }
+        [Column("Time_Stamp")]
+        public Byte[] TimeStamp { get; set; }
+     }
+}
