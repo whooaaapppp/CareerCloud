b8c3dd6a28dff74b4223aef01cc46a2c8d4d2cce
whooaaapppp <jwarain@me.com>
2019-11-19 23:28:57 -0500

    updated SecurityLoginPoco unit test fix

--- CareerCloud.Pocos/SecurityLoginPoco.cs
+++ CareerCloud.Pocos/SecurityLoginPoco.cs
@@ -21,5 +21,19 @@ namespace CareerCloud.Pocos
         public DateTime? AgreementAccepted { get; set; }
         [Column("Is_Locked")]
         public bool IsLocked { get; set; }
+        [Column("Is_Inactive")]
+        public bool IsInactive { get; set; }
+        [Column("Email_Address")]
+        public string EmailAddress { get; set; }
+        [Column("Phone_Number")]
+        public string PhoneNumber { get; set; }
+        [Column("Full_Name")]
+        public string FullName { get; set; }
+        [Column("Force_Change_Password")]
+        public bool ForceChangePassword { get; set; }
+        [Column("Prefferred_Language")]
+        public string PrefferredLanguage { get; set; }
+        [Column("Time_Stamp")]
+        public Byte[] TimeStamp { get; set; }
     }
 }
