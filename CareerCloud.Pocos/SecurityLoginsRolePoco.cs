5b1d1e480b50cd46c6889eae0e86951b0b117f30
whooaaapppp <jwarain@me.com>
2019-11-20 00:28:10 -0500

    added Securities and System rel pocos

---
+++ CareerCloud.Pocos/SecurityLoginsRolePoco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table("Security_Logins_Roles")]
+    public class SecurityLoginsRolePoco : IPoco
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid Login { get; set; }
+        public Guid Role { get; set; }
+        [Column("Time_Stamp")]
+        public Byte[] TimeStamp { get; set; }
+
+    }
+}
---
+++ CareerCloud.Pocos/SystemLanguageCodePoco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareerCloud.Pocos
+{
+    [Table("System_Language_Codes")]
+    public class SystemLanguageCodePoco
+    {
+        [Key]
+        public string LanguageID { get; set; }
+        public string Name { get; set; }
+        [Column("Native_Name")]
+        public string NativeName { get; set; }
+    }
+}
