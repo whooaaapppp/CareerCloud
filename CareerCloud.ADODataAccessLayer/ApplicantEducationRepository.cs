582c94543d27086e02bec5cdfb7e9b1b00868a5f
whooaaapppp <jwarain@me.com>
2019-11-28 23:47:13 -0500

    adding Interface implementation explicitly

--- CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
@@ -2,12 +2,46 @@
 using CareerCloud.Pocos;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace CareerCloud.ADODataAccessLayer
 {
     public class ApplicantEducationRepository : IDataRepository<ApplicantEducationPoco>
     {
-        
+        public void Add(params ApplicantEducationPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantEducationPoco> GetAll(params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantEducationPoco> GetList(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicantEducationPoco GetSingle(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params ApplicantEducationPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params ApplicantEducationPoco[] items)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
