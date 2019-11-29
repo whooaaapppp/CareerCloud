6c7dbf7d7b574ebcfe88f420d44ccb7bd44d295c
whooaaapppp <jwarain@me.com>
2019-11-28 23:45:45 -0500

    adding ADODataAccessLayer, Data Access Layer, IDataRepository

---
+++ CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
@@ -0,0 +1,13 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class ApplicantEducationRepository : IDataRepository<ApplicantEducationPoco>
+    {
+        
+    }
+}
