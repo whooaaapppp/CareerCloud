aaae612073b9a7c1c587cc0cb560e9e48ae2ba97
whooaaapppp <jwarain@me.com>
2019-12-01 13:33:39 -0500

    adding ApplicantResumeRepo, interface template, ctor params with sql cxn

---
+++ CareerCloud.ADODataAccessLayer/ApplicantResumeRepository.cs
@@ -0,0 +1,61 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class ApplicantResumeRepository : IDataRepository<ApplicantResumePoco>
+    {
+        protected readonly string _connstr;
+        public ApplicantResumeRepository()
+        {
+            //create Config Builder
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            _connstr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+        }
+
+        public void Add(params ApplicantResumePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantResumePoco> GetAll(params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ApplicantResumePoco> GetList(Expression<Func<ApplicantResumePoco, bool>> where, params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicantResumePoco GetSingle(Expression<Func<ApplicantResumePoco, bool>> where, params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params ApplicantResumePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params ApplicantResumePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
