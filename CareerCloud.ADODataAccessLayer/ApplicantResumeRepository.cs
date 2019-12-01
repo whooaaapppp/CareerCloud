d30f953079c0d5519ef1512c70471cb992755317
whooaaapppp <jwarain@me.com>
2019-12-01 13:45:56 -0500

    +Add method to AppResumeRepo

--- CareerCloud.ADODataAccessLayer/ApplicantResumeRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantResumeRepository.cs
@@ -25,35 +25,75 @@ namespace CareerCloud.ADODataAccessLayer
 
         public void Add(params ApplicantResumePoco[] items)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+
+                foreach (ApplicantResumePoco item in items)
+                {
+                    comm.CommandText = @"INSERT INTO [dbo].[Applicant_Resumes]( [Id], [Applicant], [Resume], [Last_Updated] )
+                                        VALUES( @Id, @Applicant, @Resume, @Last_Updated )";
+                    comm.Parameters.AddWithValue("@Id", item.Id);
+                    comm.Parameters.AddWithValue("@Applicant", item.Applicant);
+                    comm.Parameters.AddWithValue("@Resume", item.Resume);
+                    comm.Parameters.AddWithValue("@Last_Updated", item.LastUpdated);
+                    //sql open execute connection sequence
+                    connection.Open();
+                    int rowAffected = comm.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+        
+
+        
+
+        public IList<ApplicantResumePoco> GetAll(params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
+        {
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+
+            } 
         }
 
+        
+
+        public ApplicantResumePoco GetSingle(Expression<Func<ApplicantResumePoco, bool>> where, params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
+        {
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+            }
+        }
+
+        public void Remove(params ApplicantResumePoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+            }
+        }
+
+        public void Update(params ApplicantResumePoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+            }
+        }
+        /* unimplemented interface methods for future iterations */
         public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
         {
             throw new NotImplementedException();
         }
 
-        public IList<ApplicantResumePoco> GetAll(params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
-        {
-            throw new NotImplementedException();
-        }
-
         public IList<ApplicantResumePoco> GetList(Expression<Func<ApplicantResumePoco, bool>> where, params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ApplicantResumePoco GetSingle(Expression<Func<ApplicantResumePoco, bool>> where, params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Remove(params ApplicantResumePoco[] items)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(params ApplicantResumePoco[] items)
         {
             throw new NotImplementedException();
         }
