656066aed829e16b2ee2901aa135cb9e3cf93a28
whooaaapppp <jwarain@me.com>
2019-12-01 17:53:56 -0500

    +GetSingle -> CompanyJobSkillRepo

--- CareerCloud.ADODataAccessLayer/CompanyJobSkillRepository.cs
+++ CareerCloud.ADODataAccessLayer/CompanyJobSkillRepository.cs
@@ -39,7 +39,10 @@ namespace CareerCloud.ADODataAccessLayer
 
         public CompanyJobSkillPoco GetSingle(Expression<Func<CompanyJobSkillPoco, bool>> where, params Expression<Func<CompanyJobSkillPoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            /* https://docs.microsoft.com/en-us/dotnet/api/system.linq.iqueryable-1?view=netframework-4.8 */
+            IQueryable<CompanyJobSkillPoco> companyJobSkillPocos = GetAll().AsQueryable();
+            /* return first element of a sequence or a default value if the seq contains no elements that satisfy the where predicate */
+            return companyJobSkillPocos.Where(where).FirstOrDefault();
         }
 
         public void Remove(params CompanyJobSkillPoco[] items)
