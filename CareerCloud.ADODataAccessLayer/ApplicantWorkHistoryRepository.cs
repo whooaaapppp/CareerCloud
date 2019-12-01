1451aa61c188a62c27338c6af08533c0ff6d1f9c
whooaaapppp <jwarain@me.com>
2019-12-01 15:55:15 -0500

    +GetAll AppWorkHistoryRepo

--- CareerCloud.ADODataAccessLayer/ApplicantWorkHistoryRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantWorkHistoryRepository.cs
@@ -61,12 +61,37 @@ namespace CareerCloud.ADODataAccessLayer
             {
                 SqlCommand comm = new SqlCommand();
                 comm.Connection = connection;
-                
+                comm.CommandText = @"SELECT [Id], [Applicant], [Company_Name], [Country_Code], [Location], [Job_Title], [Job_Description], [Start_Month], [Start_Year], [End_Month], [End_Year], [Time_Stamp]
+                                    FROM [dbo].[Applicant_Work_History]";
+                connection.Open();
+                int index = 0;
+                SqlDataReader sqlReader = comm.ExecuteReader();
+                ApplicantWorkHistoryPoco[] appWorkHistoryPocos = new ApplicantWorkHistoryPoco[1000];
+                //while sqlreader has something to read
+                while (sqlReader.Read())
+                {
+                    ApplicantWorkHistoryPoco appWorkHistoryPoco = new ApplicantWorkHistoryPoco();
+                    appWorkHistoryPoco.Id = sqlReader.GetGuid(0);
+                    appWorkHistoryPoco.Applicant = sqlReader.GetGuid(1);
+                    appWorkHistoryPoco.CompanyName = sqlReader.GetString(2);
+                    appWorkHistoryPoco.CountryCode = sqlReader.GetString(3);
+                    appWorkHistoryPoco.Location = sqlReader.GetString(4);
+                    appWorkHistoryPoco.JobTitle = sqlReader.GetString(5);
+                    appWorkHistoryPoco.JobDescription = sqlReader.GetString(6);
+                    appWorkHistoryPoco.StartMonth = sqlReader.GetInt16(7);
+                    appWorkHistoryPoco.StartYear = sqlReader.GetInt32(8);
+                    appWorkHistoryPoco.EndMonth = sqlReader.GetInt16(9);
+                    appWorkHistoryPoco.EndYear = sqlReader.GetInt32(10);
+                    appWorkHistoryPoco.TimeStamp = (byte[])sqlReader[11];
+                    appWorkHistoryPocos[index] = appWorkHistoryPoco;
+                    index++;
+
+                }
+                connection.Close();
+                //return only non-null poco object -> a and pass it to list
+                return appWorkHistoryPocos.Where(a => a != null).ToList();
             }
         }
-
-        
-
         public ApplicantWorkHistoryPoco GetSingle(Expression<Func<ApplicantWorkHistoryPoco, bool>> where, params Expression<Func<ApplicantWorkHistoryPoco, object>>[] navigationProperties)
         {
             /* https://docs.microsoft.com/en-us/dotnet/api/system.linq.iqueryable-1?view=netframework-4.8 */
