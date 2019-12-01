8eacec6db31156508ad9e0800dc26092b0e9986a
whooaaapppp <jwarain@me.com>
2019-11-30 23:48:57 -0500

    added ApplicantProfileRepo with ctor and interface template

---
+++ CareerCloud.ADODataAccessLayer/ApplicantProfileRepository.cs
@@ -0,0 +1,65 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class ApplicantProfileRepository : IDataRepository<ApplicantProfilePoco>
+    {
+        protected readonly string _connstr;
+        public ApplicantProfileRepository()
+        {
+            //create Config Builder
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            _connstr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+        }
+        
+        public void Add(params ApplicantProfilePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public IList<ApplicantProfilePoco> GetAll(params Expression<Func<ApplicantProfilePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public ApplicantProfilePoco GetSingle(Expression<Func<ApplicantProfilePoco, bool>> where, params Expression<Func<ApplicantProfilePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params ApplicantProfilePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params ApplicantProfilePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* unimplemented interface methods for future iterations */
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+        public IList<ApplicantProfilePoco> GetList(Expression<Func<ApplicantProfilePoco, bool>> where, params Expression<Func<ApplicantProfilePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
