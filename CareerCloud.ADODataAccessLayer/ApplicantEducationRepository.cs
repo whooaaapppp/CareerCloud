b5154f1cd1fa4faad8ace43066297a90cfe949b0
whooaaapppp <jwarain@me.com>
2019-11-30 21:19:25 -0500

    added linq, expression lib, implement getall list

--- CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
@@ -5,7 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
+using System.Linq;
 using System.Linq.Expressions;
+
 
 namespace CareerCloud.ADODataAccessLayer
 {
@@ -51,7 +53,7 @@ namespace CareerCloud.ADODataAccessLayer
                                          @Completion_Date,
                                          @Completion_Percent
                                         )";
-                    comm.Parameters.AddWithValue("@Id",item.Id);
+                    comm.Parameters.AddWithValue("@Id", item.Id);
                     comm.Parameters.AddWithValue("@Applicant", item.Applicant);
                     comm.Parameters.AddWithValue("@Major", item.Major);
                     comm.Parameters.AddWithValue("@Certificate_Diploma", item.CertificateDiploma);
@@ -62,9 +64,11 @@ namespace CareerCloud.ADODataAccessLayer
                     connection.Open();
                     int rowAffected = comm.ExecuteNonQuery();
                     connection.Close();
-                    
+
                 }
             }
+
+
         }
 
         public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
@@ -74,7 +78,45 @@ namespace CareerCloud.ADODataAccessLayer
 
         public IList<ApplicantEducationPoco> GetAll(params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            //getall list on the poco
+
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+                comm.CommandText = @"SELECT [Id]
+                                  ,[Applicant]
+                                  ,[Major]
+                                  ,[Certificate_Diploma]
+                                  ,[Start_Date]
+                                  ,[Completion_Date]
+                                  ,[Completion_Percent]
+                                  ,[Time_Stamp]
+                                   FROM [dbo].[Applicant_Educations]";
+                connection.Open();
+                int index = 0;
+                SqlDataReader sqlReader = comm.ExecuteReader();
+                ApplicantEducationPoco[] appEduPocos = new ApplicantEducationPoco[500];
+                //while has sql reader to read
+                while (sqlReader.Read())
+                {
+                    ApplicantEducationPoco appEduPoco = new ApplicantEducationPoco();
+                    appEduPoco.Id = sqlReader.GetGuid(0);
+                    appEduPoco.Applicant = sqlReader.GetGuid(1);
+                    appEduPoco.Major = sqlReader.GetString(2);
+                    appEduPoco.CertificateDiploma = sqlReader.GetString(3);
+                    appEduPoco.StartDate = (DateTime?)sqlReader.GetDateTime(4);
+                    appEduPoco.CompletionDate = (DateTime?)sqlReader.GetDateTime(5);
+                    appEduPoco.CompletionPercent = (byte?)sqlReader[6];
+                    appEduPoco.TimeStamp = (byte[])sqlReader[7];
+                    appEduPocos[index] = appEduPoco;
+                    index++;
+                }
+
+                return appEduPocos.Where(a => a != null).ToList();
+
+
+            }
         }
 
         public IList<ApplicantEducationPoco> GetList(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
@@ -84,6 +126,7 @@ namespace CareerCloud.ADODataAccessLayer
 
         public ApplicantEducationPoco GetSingle(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
         {
+
             throw new NotImplementedException();
         }
 
