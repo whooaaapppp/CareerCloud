899cb98848a968e6edd14cc4a51effcaf9b0bb15
whooaaapppp <jwarain@me.com>
2019-12-01 20:47:01 -0500

    +GetSingle -> CompanyProfileRepo, updated comming sql cxn handlers and foreach list

--- CareerCloud.ADODataAccessLayer/CompanyProfileRepository.cs
+++ CareerCloud.ADODataAccessLayer/CompanyProfileRepository.cs
@@ -12,42 +12,104 @@ namespace CareerCloud.ADODataAccessLayer
 {
     public class CompanyProfileRepository : IDataRepository<CompanyProfilePoco>
     {
+        protected readonly string _connstr;
+
         public CompanyProfileRepository()
         {
-
+            //create Config Builder
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            _connstr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
         }
 
         public void Add(params CompanyProfilePoco[] items)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+
+                foreach(CompanyProfilePoco item in items)
+                {
+                    //prepping the sql query
+                    comm.CommandText = @"";
+                    comm.Parameters.AddWithValue("@Id", item.Id);
+                    //rows affected
+                    connection.Open();
+                    int rowAffected = comm.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
         }
 
-        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
-        {
-            throw new NotImplementedException();
-        }
+        
 
         public IList<CompanyProfilePoco> GetAll(params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+            }
         }
 
-        public IList<CompanyProfilePoco> GetList(Expression<Func<CompanyProfilePoco, bool>> where, params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
-        {
-            throw new NotImplementedException();
-        }
+        
 
         public CompanyProfilePoco GetSingle(Expression<Func<CompanyProfilePoco, bool>> where, params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            /* https://docs.microsoft.com/en-us/dotnet/api/system.linq.iqueryable-1?view=netframework-4.8 */
+            IQueryable<CompanyProfilePoco> companyProfilePocos = GetAll().AsQueryable();
+            /* return first element of a sequence or a default value if the seq contains no elements that satisfy the where predicate */
+            return companyProfilePocos.Where(where).FirstOrDefault();
         }
 
         public void Remove(params CompanyProfilePoco[] items)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+
+                foreach (CompanyProfilePoco item in items)
+                {
+                    //prepping the sql query
+                    comm.CommandText = @"";
+                    comm.Parameters.AddWithValue("@Id", item.Id);
+                    //rows affected
+                    connection.Open();
+                    int rowAffected = comm.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
         }
 
         public void Update(params CompanyProfilePoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+
+                foreach (CompanyProfilePoco item in items)
+                {
+                    //prepping the sql query
+                    comm.CommandText = @"";
+                    comm.Parameters.AddWithValue("@Id", item.Id);
+                    //rows affected
+                    connection.Open();
+                    int rowAffected = comm.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+        /* unimplemented interface methods for future iterations */
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+        public IList<CompanyProfilePoco> GetList(Expression<Func<CompanyProfilePoco, bool>> where, params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
         {
             throw new NotImplementedException();
         }
