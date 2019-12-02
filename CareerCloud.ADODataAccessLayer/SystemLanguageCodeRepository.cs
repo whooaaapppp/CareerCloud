f8f0b3d93b497d421b2beea87ac394c61ebeae4e
whooaaapppp <jwarain@me.com>
2019-12-02 00:26:21 -0500

    +SystemLanguageCodeRepo, init interface template, deps, sql hdn

---
+++ CareerCloud.ADODataAccessLayer/SystemLanguageCodeRepository.cs
@@ -0,0 +1,67 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class SystemLanguageCodeRepository : IDataRepository<SystemLanguageCodePoco>
+    {
+        protected readonly string _connstr;
+        public SystemLanguageCodeRepository()
+        {
+            //create Config Builder
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            _connstr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+        }
+
+        public void Add(params SystemLanguageCodePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public IList<SystemLanguageCodePoco> GetAll(params Expression<Func<SystemLanguageCodePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public SystemLanguageCodePoco GetSingle(Expression<Func<SystemLanguageCodePoco, bool>> where, params Expression<Func<SystemLanguageCodePoco, object>>[] navigationProperties)
+        {
+            /* https://docs.microsoft.com/en-us/dotnet/api/system.linq.iqueryable-1?view=netframework-4.8 */
+            IQueryable<SystemLanguageCodePoco> systemLanguageCodePocos = GetAll().AsQueryable();
+            /* return first element of a sequence or a default value if the seq contains no elements that satisfy the where predicate */
+            return systemLanguageCodePocos.Where(where).FirstOrDefault();
+        }
+
+        public void Remove(params SystemLanguageCodePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params SystemLanguageCodePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+        public IList<SystemLanguageCodePoco> GetList(Expression<Func<SystemLanguageCodePoco, bool>> where, params Expression<Func<SystemLanguageCodePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
