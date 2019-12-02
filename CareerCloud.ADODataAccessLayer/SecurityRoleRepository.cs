bcc872f995c2dce3d6b5abb3fcf22df04c39b640
whooaaapppp <jwarain@me.com>
2019-12-01 23:33:20 -0500

    +SecurityRoleRepo, init interface template, libs, sqlhandlers

---
+++ CareerCloud.ADODataAccessLayer/SecurityRoleRepository.cs
@@ -0,0 +1,80 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class SecurityRoleRepository : IDataRepository<SecurityRolePoco>
+    {
+        protected readonly string _connstr;
+        public SecurityRoleRepository()
+        {
+            //create Config Builder
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            _connstr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+        }
+
+        public void Add(params SecurityRolePoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+            }
+        }
+
+        
+
+        public IList<SecurityRolePoco> GetAll(params Expression<Func<SecurityRolePoco, object>>[] navigationProperties)
+        {
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+            }
+        }
+
+        
+
+        public SecurityRolePoco GetSingle(Expression<Func<SecurityRolePoco, bool>> where, params Expression<Func<SecurityRolePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params SecurityRolePoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+            }
+        }
+
+        public void Update(params SecurityRolePoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+            }
+        }
+        /* unimplemented interface methods for future iterations */
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+        public IList<SecurityRolePoco> GetList(Expression<Func<SecurityRolePoco, bool>> where, params Expression<Func<SecurityRolePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
