ddbc164e09b7eb53deb0d89a8b7eee2d86107e7b
whooaaapppp <jwarain@me.com>
2019-12-02 00:42:28 -0500

    fix build issues

--- CareerCloud.ADODataAccessLayer/SystemCountryCodeRepository.cs
+++ CareerCloud.ADODataAccessLayer/SystemCountryCodeRepository.cs
@@ -10,7 +10,7 @@ using System.Linq.Expressions;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    protected readonly string _connstr;
+    
     public class SystemCountryCodeRepository : IDataRepository<SystemCountryCodePoco>
     {
         protected readonly string _connstr;
