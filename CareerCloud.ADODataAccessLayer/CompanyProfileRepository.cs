0575d0cd47f419163f0e0e169f03b66e2c722874
whooaaapppp <jwarain@me.com>
2019-12-01 21:15:54 -0500

    +GetAll -> IsDBNull handling ternary operators, CompanyProfileRepo

--- CareerCloud.ADODataAccessLayer/CompanyProfileRepository.cs
+++ CareerCloud.ADODataAccessLayer/CompanyProfileRepository.cs
@@ -49,20 +49,42 @@ namespace CareerCloud.ADODataAccessLayer
                 }
             }
         }
-
-        
-
         public IList<CompanyProfilePoco> GetAll(params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
         {
             using (SqlConnection connection = new SqlConnection(_connstr))
             {
                 SqlCommand comm = new SqlCommand();
                 comm.Connection = connection;
+                comm.CommandText = @"SELECT [Id], [Registration_Date], [Company_Website], [Contact_Phone], [Contact_Name], [Company_Logo], [Time_Stamp]
+                                    FROM [dbo].[Company_Profiles]";
+                connection.Open();
+                int index = 0;
+                SqlDataReader sqlReader = comm.ExecuteReader();
+                CompanyProfilePoco[] companyProfilePocos = new CompanyProfilePoco[1000];
+                //while sqlreader has something to read
+                while (sqlReader.Read())
+                {
+                    CompanyProfilePoco companyProfilePoco = new CompanyProfilePoco();
+                    companyProfilePoco.Id = sqlReader.GetGuid(0);
+                    companyProfilePoco.RegistrationDate = sqlReader.GetDateTime(1);
+                    //handling nullable var entries to empty strings
+                    companyProfilePoco.CompanyWebsite = sqlReader.IsDBNull(2)? String.Empty : sqlReader.GetString(2);
+                    companyProfilePoco.ContactPhone = sqlReader.GetString(3);
+                    //handling nullable var entries to empty strings
+                    companyProfilePoco.ContactName =sqlReader.IsDBNull(4)? String.Empty : sqlReader.GetString(4);
+                    if (!sqlReader.IsDBNull(5)){
+                        companyProfilePoco.CompanyLogo = (byte[])sqlReader[5];
+                    }
+                    companyProfilePoco.TimeStamp = (byte[])sqlReader[6];
+                    companyProfilePocos[index] = companyProfilePoco;
+                    index++;
+                }
+                connection.Close();
+                //return only non-null poco object -> a and pass it to list
+                return companyProfilePocos.Where(a => a != null).ToList();
+
             }
         }
-
-        
-
         public CompanyProfilePoco GetSingle(Expression<Func<CompanyProfilePoco, bool>> where, params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
         {
             /* https://docs.microsoft.com/en-us/dotnet/api/system.linq.iqueryable-1?view=netframework-4.8 */
