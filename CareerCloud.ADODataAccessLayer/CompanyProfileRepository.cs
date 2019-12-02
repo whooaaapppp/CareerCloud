dc261e3cfa9eca42c77c1bc31b6cd67abed05a15
whooaaapppp <jwarain@me.com>
2019-12-01 20:42:41 -0500

    +CompanyProfileRepo , interface template and updated libr

---
+++ CareerCloud.ADODataAccessLayer/CompanyProfileRepository.cs
@@ -0,0 +1,55 @@
+﻿using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class CompanyProfileRepository : IDataRepository<CompanyProfilePoco>
+    {
+        public CompanyProfileRepository()
+        {
+
+        }
+
+        public void Add(params CompanyProfilePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CompanyProfilePoco> GetAll(params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CompanyProfilePoco> GetList(Expression<Func<CompanyProfilePoco, bool>> where, params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CompanyProfilePoco GetSingle(Expression<Func<CompanyProfilePoco, bool>> where, params Expression<Func<CompanyProfilePoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params CompanyProfilePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(params CompanyProfilePoco[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
