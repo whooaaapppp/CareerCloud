e0500b142e42740494c92c5f08099d1539610f74
whooaaapppp <jwarain@me.com>
2019-12-01 21:41:45 -0500

    +Remove -> SecurityLoginRepo

--- CareerCloud.ADODataAccessLayer/SecurityLoginRepository.cs
+++ CareerCloud.ADODataAccessLayer/SecurityLoginRepository.cs
@@ -24,35 +24,92 @@ namespace CareerCloud.ADODataAccessLayer
 
         public void Add(params SecurityLoginPoco[] items)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+
+                foreach (SecurityLoginPoco item in items)
+                {
+                    //prepping the sql query
+                    comm.CommandText = @""
+                    comm.Parameters.AddWithValue();
+                    //rows affected
+                    connection.Open();
+                    int rowAffected = comm.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
         }
 
-        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
-        {
-            throw new NotImplementedException();
-        }
+        
 
         public IList<SecurityLoginPoco> GetAll(params Expression<Func<SecurityLoginPoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+
+                
+
+            }
         }
 
-        public IList<SecurityLoginPoco> GetList(Expression<Func<SecurityLoginPoco, bool>> where, params Expression<Func<SecurityLoginPoco, object>>[] navigationProperties)
-        {
-            throw new NotImplementedException();
-        }
+        
 
         public SecurityLoginPoco GetSingle(Expression<Func<SecurityLoginPoco, bool>> where, params Expression<Func<SecurityLoginPoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            /* https://docs.microsoft.com/en-us/dotnet/api/system.linq.iqueryable-1?view=netframework-4.8 */
+            IQueryable<SecurityLoginPoco> securityLoginPocos = GetAll().AsQueryable();
+            /* return first element of a sequence or a default value if the seq contains no elements that satisfy the where predicate */
+            return securityLoginPocos.Where(where).FirstOrDefault();
         }
 
         public void Remove(params SecurityLoginPoco[] items)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+                foreach (SecurityLoginPoco item in items)
+                {
+                    //prepping the sql query
+                    comm.CommandText = @"DELETE FROM [dbo].[Security_Logins]
+                                         WHERE [Id] = @Id";
+                    comm.Parameters.AddWithValue("@Id", item.Id);
+                    //rows affected
+                    connection.Open();
+                    int rowAffected = comm.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
         }
 
         public void Update(params SecurityLoginPoco[] items)
+        {
+            using (SqlConnection connection = new SqlConnection(_connstr))
+            {
+                SqlCommand comm = new SqlCommand();
+                comm.Connection = connection;
+                foreach (SecurityLoginPoco item in items)
+                {
+                    //prepping the sql query
+                    comm.CommandText = @""
+                    comm.Parameters.AddWithValue();
+                    //rows affected
+                    connection.Open();
+                    int rowAffected = comm.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+        }
+        /* unimplemented interface methods for future iterations */
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+        public IList<SecurityLoginPoco> GetList(Expression<Func<SecurityLoginPoco, bool>> where, params Expression<Func<SecurityLoginPoco, object>>[] navigationProperties)
         {
             throw new NotImplementedException();
         }
