a226a220151333664ddc12d8e6c0a94ced561ee4
whooaaapppp <jwarain@me.com>
2019-12-31 00:07:53 -0500

    +ApplicantResumeLogic : +add,update,verify

---
+++ CareerCloud.BusinessLogicLayer/ApplicantResumeLogic.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantResumeLogic : BaseLogic<ApplicantResumePoco>
+    {
+        public ApplicantResumeLogic(IDataRepository<ApplicantResumePoco> repository) : base(repository)
+
+        {
+
+        }
+        protected override void Verify(ApplicantResumePoco[] pocos)
+        {
+            //implement the exception rules 113
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (ApplicantResumePoco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.Resume))
+                {
+                    exceptions.Add(new ValidationException(113, "Resume cannot be empty. Please supply resume"));
+                }
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+        public override void Add(ApplicantResumePoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantResumePoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Update(pocos);
+        }
+    }
+}
