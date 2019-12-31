d09c253fd3fdc3bb3b40d18adc68501dd82b3c3f
whooaaapppp <jwarain@me.com>
2019-12-31 09:31:59 -0500

    +CompanyJobSkillLogic : +add,verify,update

---
+++ CareerCloud.BusinessLogicLayer/CompanyJobSkillLogic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyJobSkillLogic : BaseLogic<CompanyJobSkillPoco>
+    {
+        public CompanyJobSkillLogic(IDataRepository<CompanyJobSkillPoco> repository) : base(repository)
+        {
+
+        }
+
+        protected override void Verify(CompanyJobSkillPoco[] pocos)
+        {
+            //implement the exception rules 400
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (CompanyJobSkillPoco poco in pocos)
+            {
+                if (poco.Importance < 0)
+                {
+                    exceptions.Add(new ValidationException(400, $"Importance {poco.Importance} cannot be less than 0"));
+                }
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+        public override void Add(CompanyJobSkillPoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyJobSkillPoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+
+    }
+}
