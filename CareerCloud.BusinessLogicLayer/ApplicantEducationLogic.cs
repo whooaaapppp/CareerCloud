a3929b74cd4074fd75a29ca732cb7b42b809e807
whooaaapppp <jwarain@me.com>
2019-12-30 21:22:42 -0500

    +ApplicantEducationLogic : implement rules exceptions and baseLogic override methods

---
+++ CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantEducationLogic : BaseLogic<ApplicantEducationPoco>
+    {
+        public ApplicantEducationLogic(IDataRepository<ApplicantEducationPoco> repository) : base(repository)
+        {
+
+        }
+        protected override void Verify(ApplicantEducationPoco[] pocos)
+        {
+            //implement the exception rules 107,108,109
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (ApplicantEducationPoco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.Major))
+                {
+                    exceptions.Add(new ValidationException(107, "Major field blank. Please enter Major"));
+                }
+                else if (poco.Major.Length < 3)
+                {
+                    exceptions.Add(new ValidationException(107, "Major is less than 3 characters."));
+                }
+
+                if (poco.StartDate > DateTime.Now)
+                {
+                    exceptions.Add(new ValidationException(108, "Start date is greater than today!"));
+                }
+
+                if (poco.CompletionDate < poco.StartDate)
+                {
+                    exceptions.Add(new ValidationException(109, "Completion date cannot be earlier than start date!"));
+                }
+            }
+            //check exception count 
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+
+        public override void Add(ApplicantEducationPoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantEducationPoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+    }
+}
