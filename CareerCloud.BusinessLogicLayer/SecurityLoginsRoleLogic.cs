bb68dd0d8a25cf68fd852363e855887266df2c9d
whooaaapppp <jwarain@me.com>
2019-12-31 11:26:00 -0500

    +fix remain Utest issues, SecurityLoginsRoleLogic : +add,verify,update

---
+++ CareerCloud.BusinessLogicLayer/SecurityLoginsRoleLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class SecurityLoginsRoleLogic : BaseLogic<SecurityLoginsRolePoco>
+    {
+        public SecurityLoginsRoleLogic(IDataRepository<SecurityLoginsRolePoco> repository) : base(repository)
+        {
+
+        }
+        protected override void Verify(SecurityLoginsRolePoco[] pocos)
+        {
+            //implement the exception rules as defined
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (SecurityLoginsRolePoco poco in pocos)
+            {
+                //for future iterations
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+        public override void Add(SecurityLoginsRolePoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(SecurityLoginsRolePoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+    }
+}
