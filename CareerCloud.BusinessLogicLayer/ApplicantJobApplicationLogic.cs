096c72ae720851528a3589eb3a711778c57438e7
whooaaapppp <jwarain@me.com>
2019-12-30 23:47:05 -0500

    +ApplicantProfileLogic : +add,update,verify baselogic methods

---
+++ CareerCloud.BusinessLogicLayer/ApplicantJobApplicationLogic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantJobApplicationLogic : BaseLogic<ApplicantJobApplicationPoco>
+    {
+        public ApplicantJobApplicationLogic(IDataRepository<ApplicantJobApplicationPoco> repository) : base(repository)    
+        {
+        }
+        protected override void Verify(ApplicantJobApplicationPoco[] pocos)
+        {
+            //implement the exception rules 110
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (ApplicantJobApplicationPoco poco in pocos)
+            {
+                if(poco.ApplicationDate > DateTime.Now)
+                {
+                    exceptions.Add(new ValidationException(110, "ApplicationDate cannot be greater than today!"));
+                }
+            }
+
+            if(exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+        public override void Add(ApplicantJobApplicationPoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(ApplicantJobApplicationPoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Update(pocos);
+        }
+
+    }
+}
