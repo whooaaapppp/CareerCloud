9182b20ff62d43d48dde025c80547d8dd4d2e80e
whooaaapppp <jwarain@me.com>
2019-12-31 09:00:45 -0500

    +CompanyJobEducationLogic : +add,verify,update

---
+++ CareerCloud.BusinessLogicLayer/CompanyJobEducationLogic.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyJobEducationLogic : BaseLogic<CompanyJobEducationPoco>
+    {
+        public CompanyJobEducationLogic(IDataRepository<CompanyJobEducationPoco> repository) : base(repository)
+        {
+                
+        }
+
+        protected override void Verify(CompanyJobEducationPoco[] pocos)
+        {
+            //implement the exception rules 200,201
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (CompanyJobEducationPoco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.Major))
+                {
+                    exceptions.Add(new ValidationException(200, "Major field blank. Please enter Major"));
+                }
+                else if (poco.Major.Length < 2)
+                {
+                    exceptions.Add(new ValidationException(200, "Major must be at least 2 characters."));
+                }
+
+                if (poco.Importance < 0)
+                {
+                    exceptions.Add(new ValidationException(201, "Importance cannot be less than 0"));
+                }
+
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+        public override void Add(CompanyJobEducationPoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyJobEducationPoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Update(pocos);
+        }
+    }
+}
