3793fcd6af7a9a9d8e74d13b8d89b20c8029cf9f
whooaaapppp <jwarain@me.com>
2019-12-31 07:46:44 -0500

    +CompanyJobDescriptionLogic : +add,verify,update

---
+++ CareerCloud.BusinessLogicLayer/CompanyJobDescriptionLogic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class CompanyJobDescriptionLogic : BaseLogic<CompanyJobDescriptionPoco>
+    {
+        public CompanyJobDescriptionLogic(IDataRepository<CompanyJobDescriptionPoco> repository) : base(repository)
+        {
+
+        }
+
+        protected override void Verify(CompanyJobDescriptionPoco[] pocos)
+        {
+            //implement the exception rules 300,301
+            List<ValidationException> exceptions = new List<ValidationException>();
+            foreach (CompanyJobDescriptionPoco poco in pocos)
+            {
+                if (string.IsNullOrEmpty(poco.JobName))
+                {
+                    exceptions.Add(new ValidationException(300, "Job Name cannot be empty. Please enter Job Name."));
+                }
+                if (string.IsNullOrEmpty(poco.JobDescriptions))
+                {
+                    exceptions.Add(new ValidationException(301, "Job Description cannot be empty. Please enter Job Description."));
+                }
+            }
+
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+        public override void Add(CompanyJobDescriptionPoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Add(pocos);
+        }
+
+        public override void Update(CompanyJobDescriptionPoco[] pocos)
+        {
+            //make sure poco is clean, doesn't have exceptions
+            Verify(pocos);
+            base.Update(pocos);
+        }
+    }
+}
