40f7c54b2a4f86dbcfb8e9230567589820010dab
whooaaapppp <jwarain@me.com>
2019-12-30 21:52:15 -0500

    +create SystemLanguageCodeLogic : don't use IPoco constraint

---
+++ CareerCloud.BusinessLogicLayer/SystemLanguageCodeLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class SystemLanguageCodeLogic
+    {
+
+    }
+}
