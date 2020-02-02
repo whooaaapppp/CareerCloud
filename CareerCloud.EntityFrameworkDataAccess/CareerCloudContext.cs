6c38c204603a2492660e3bdb76c286c1fdf6f03f
whooaaapppp <jwarain@me.com>
2020-02-02 13:44:25 -0500

    +cctask UT,+cc.EntityFrameworkDataAccess,+CareerCloudContext,+EFGenericRepository

---
+++ CareerCloud.EntityFrameworkDataAccess/CareerCloudContext.cs
@@ -0,0 +1,33 @@
+﻿using CareerCloud.Pocos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CareerCloud.EntityFrameworkDataAccess
+{
+    class CareerCloudContext : DbContext
+    {
+        public DbSet<ApplicantEducationPoco> ApplicantEducations { get; set; }
+        
+        
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //create Config Builder
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            string _connstr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+
+            //pass db sql config on the optionsBuilder
+            optionsBuilder.UseSqlServer(_connstr);
+
+            base.OnConfiguring(optionsBuilder);
+
+        }
+    }
+}
---
+++ CareerCloud.EntityFrameworkDataAccess/EFGenericRepository.cs
@@ -0,0 +1,63 @@
+﻿using CareerCloud.DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+
+namespace CareerCloud.EntityFrameworkDataAccess
+{
+    class EFGenericRepository<T> : IDataRepository<T> where T : class
+    {
+        private CareerCloudContext _context;
+        
+        public EFGenericRepository()
+        {
+            _context = new CareerCloudContext();
+        }
+        
+        public void Add(params T[] items)
+        {
+            //array T and loop through the array and foreach of those since entry is one to one and set the enum entity state added ~4
+            foreach(T item in items)
+            {
+                _context.Entry(item).State = EntityState.Added;
+            }
+        }
+
+        //future iterations
+        public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> GetAll(params Expression<Func<T, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> GetList(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params T[] items)
+        {
+            foreach (T item in items)
+            {
+                _context.Entry(item).State = EntityState.Deleted;
+            }
+        }
+
+        public void Update(params T[] items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
