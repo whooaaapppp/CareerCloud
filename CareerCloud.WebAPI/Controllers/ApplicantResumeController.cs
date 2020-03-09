a1bd0f01b3ea05975484b8a66a17d1b1570570a9
whooaaapppp <jwarain@me.com>
2020-03-08 23:20:38 -0400

    +ApplicantResumeController

---
+++ CareerCloud.WebAPI/Controllers/ApplicantResumeController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/applicant/v1")]
+    [ApiController]
+    public class ApplicantResumeController : ControllerBase
+    {
+        private ApplicantResumeLogic _logic;
+        
+
+        public ApplicantResumeController()
+        {
+            var repo = new EFGenericRepository<ApplicantResumePoco>();
+            _logic = new ApplicantResumeLogic(repo);
+
+
+        }
+
+        //Get on ID
+        [HttpGet]
+        [Route("profile/{id}")]
+        public ActionResult GetApplicantResume(Guid id)
+        {
+            ApplicantResumePoco poco = _logic.Get(id);
+            if (poco == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(poco);
+            }
+        }
+
+        //Get All
+        [HttpGet]
+        [Route("profile")]
+        public ActionResult GetAllApplicantResume()
+        {
+            List<ApplicantResumePoco> pocos = _logic.GetAll();
+            if (pocos == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(pocos);
+            }
+
+        }
+
+        //Post
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPost]
+        [Route("profile")]
+        public ActionResult PostApplicantResume([FromBody] ApplicantResumePoco[] applicantResumePocos)
+        {
+            _logic.Add(applicantResumePocos);
+            return Ok();
+        }
+
+        //Put
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPut]
+        [Route("profile")]
+        public ActionResult PutApplicantResume([FromBody] ApplicantResumePoco[] applicantResumePocos)
+        {
+            _logic.Update(applicantResumePocos);
+            return Ok();
+        }
+
+        //Delete
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpDelete]
+        [Route("profile")]
+        public ActionResult DeleteApplicantResume([FromBody] ApplicantResumePoco[] applicantResumePocos)
+        {
+            _logic.Delete(applicantResumePocos);
+            return Ok();
+        }
+
+
+
+
+    }
+}
