02bfd89122dc80b12e40cf731306497756b08ac6
whooaaapppp <jwarain@me.com>
2020-03-08 22:39:16 -0400

    +ApplicantJobApplicationController

--- CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
+++ CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
@@ -59,5 +59,7 @@ namespace CareerCloud.BusinessLogicLayer
             base.Update(pocos);
         }
 
+
+
     }
 }
---
+++ CareerCloud.WebAPI/Controllers/ApplicantJobApplicationController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/applicant/v1")]
+    [ApiController]
+    public class ApplicantJobApplicationController : ControllerBase
+    {
+        private readonly ApplicantJobApplicationLogic _logic;
+
+        public ApplicantJobApplicationController()
+        {
+            var repo = new EFGenericRepository<ApplicantJobApplicationPoco>();
+            _logic = new ApplicantJobApplicationLogic(repo);
+
+
+        }
+
+        //Get on ID
+        [HttpGet]
+        [Route("education/{id}")]
+        public ActionResult GetApplicantEducation(Guid id)
+        {
+            ApplicantJobApplicationPoco poco = _logic.Get(id);
+            if (poco == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(poco);
+            }
+        }
+
+        //Get All
+        [HttpGet]
+        [Route("education")]
+        public ActionResult GetAllApplicantEducation()
+        {
+            List<ApplicantJobApplicationPoco> pocos = _logic.GetAll();
+            if (pocos == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(pocos);
+            }
+
+        }
+
+        //Post
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPost]
+        [Route("education")]
+        public ActionResult PostApplicantEducation([FromBody] ApplicantJobApplicationPoco[] applicantJobApplicationPocos)
+        {
+            _logic.Add(applicantJobApplicationPocos);
+            return Ok();
+        }
+
+        //Put
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPut]
+        [Route("education")]
+        public ActionResult PutApplicantEducation([FromBody] ApplicantJobApplicationPoco[] applicantJobApplicationPocos)
+        {
+            _logic.Update(applicantJobApplicationPocos);
+            return Ok();
+        }
+
+        //Delete
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpDelete]
+        [Route("education")]
+        public ActionResult DeleteApplicantEducation([FromBody] ApplicantJobApplicationPoco[] applicantJobApplicationPocos)
+        {
+            _logic.Delete(applicantJobApplicationPocos);
+            return Ok();
+        }
+
+
+
+
+    }
+}
