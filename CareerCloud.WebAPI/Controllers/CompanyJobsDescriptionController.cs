59cc490f85169d904b3289009c549bcee8c4c74e
whooaaapppp <jwarain@me.com>
2020-03-09 00:06:20 -0400

    +companyjobsdescription controller, fixes

---
+++ CareerCloud.WebAPI/Controllers/CompanyJobsDescriptionController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/company/v1")]
+    [ApiController]
+    public class CompanyJobsDescriptionController : ControllerBase
+    {
+        private CompanyJobDescriptionLogic _logic;
+        
+
+        public CompanyJobsDescriptionController()
+        {
+            var repo = new EFGenericRepository<CompanyJobDescriptionPoco>();
+            _logic = new CompanyJobDescriptionLogic(repo);
+        }
+
+        //Get on ID
+        [HttpGet]
+        [Route("jobsdescription/{id}")]
+        public ActionResult GetCompanyJobDescription(Guid id)
+        {
+            CompanyJobDescriptionPoco poco = _logic.Get(id);
+            if (poco == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(poco);
+            }
+        }
+
+        //Get All
+        [HttpGet]
+        [Route("jobsdescription")]
+        public ActionResult GetAllCompanyJobDescription()
+        {
+            List<CompanyJobDescriptionPoco> pocos = _logic.GetAll();
+            if (pocos == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(pocos);
+            }
+
+        }
+
+        //Post
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPost]
+        [Route("jobsdescription")]
+        public ActionResult PostCompanyJobDescription([FromBody] CompanyJobDescriptionPoco[] companyJobDescriptionPocos)
+        {
+            _logic.Add(companyJobDescriptionPocos);
+            return Ok();
+        }
+
+        //Put
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPut]
+        [Route("jobsdescription")]
+        public ActionResult PutCompanyJobDescription([FromBody] CompanyJobDescriptionPoco[] companyJobDescriptionPocos)
+        {
+            _logic.Update(companyJobDescriptionPocos);
+            return Ok();
+        }
+
+        //Delete
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpDelete]
+        [Route("jobsdescription")]
+        public ActionResult DeleteCompanyJobDescription([FromBody] CompanyJobDescriptionPoco[] companyJobDescriptionPocos)
+        {
+            _logic.Delete(companyJobDescriptionPocos);
+            return Ok();
+        }
+
+    }
+}
