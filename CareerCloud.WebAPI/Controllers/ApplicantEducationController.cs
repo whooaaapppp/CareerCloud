a3d88e8058bc0011bf1c274816a566279a004957
whooaaapppp <jwarain@me.com>
2020-03-08 21:06:38 -0400

    initial controller test applicant education

--- CareerCloud.WebAPI/Controllers/ApplicantEducationController.cs
+++ CareerCloud.WebAPI/Controllers/ApplicantEducationController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +14,31 @@ namespace CareerCloud.WebAPI.Controllers
     [ApiController]
     public class ApplicantEducationController : ControllerBase
     {
+        private readonly ApplicantEducationLogic _logic;
 
+        public ApplicantEducationController()
+        {
+            var repo = new EFGenericRepository<ApplicantEducationPoco>();
+            _logic = new ApplicantEducationLogic(repo);
+
+
+        }
+
+        [HttpGet]
+        [Route("education/{id}")]
+        public ActionResult GetApplicantEducation(Guid id)
+        {
+            ApplicantEducationPoco poco = _logic.Get(id);
+            if (poco == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(poco);
+            }
+        }
     }
 }
