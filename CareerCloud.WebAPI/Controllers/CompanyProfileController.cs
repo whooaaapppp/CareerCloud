21a27164e22eea0628f6d0ab3581ed2a893b9928
whooaaapppp <jwarain@me.com>
2020-03-09 00:27:10 -0400

    +CompanyProfile controller and other fixes

---
+++ CareerCloud.WebAPI/Controllers/CompanyProfileController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/company/v1")]
+    [ApiController]
+    public class CompanyProfileController : ControllerBase
+    {
+        private CompanyProfileLogic _logic;
+        
+
+        public CompanyProfileController()
+        {
+            var repo = new EFGenericRepository<CompanyProfilePoco>();
+            _logic = new CompanyProfileLogic(repo);
+        }
+
+        //Get on ID
+        [HttpGet]
+        [Route("profile/{id}")]
+        public ActionResult GetCompanyProfile(Guid id)
+        {
+            CompanyProfilePoco poco = _logic.Get(id);
+            if (poco == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(poco);
+            }
+        }
+
+        //Get All
+        [HttpGet]
+        [Route("profile")]
+        public ActionResult GetAllCompanyProfile()
+        {
+            List<CompanyProfilePoco> pocos = _logic.GetAll();
+            if (pocos == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(pocos);
+            }
+
+        }
+
+        //Post
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPost]
+        [Route("profile")]
+        public ActionResult PostCompanyProfile([FromBody] CompanyProfilePoco[] companyProfilePocos)
+        {
+            _logic.Add(companyProfilePocos);
+            return Ok();
+        }
+
+        //Put
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPut]
+        [Route("profile")]
+        public ActionResult PutCompanyProfile([FromBody] CompanyProfilePoco[] companyProfilePocos)
+        {
+            _logic.Update(companyProfilePocos);
+            return Ok();
+        }
+
+        //Delete
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpDelete]
+        [Route("profile")]
+        public ActionResult DeleteCompanyProfile([FromBody] CompanyProfilePoco[] companyProfilePocos)
+        {
+            _logic.Delete(companyProfilePocos);
+            return Ok();
+        }
+
+    }
+}
