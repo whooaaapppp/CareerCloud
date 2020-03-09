091a72c920f2251b5db73098b4c87e24f8606ea5
whooaaapppp <jwarain@me.com>
2020-03-09 00:19:17 -0400

    +CompanyLocation controller and fixes

---
+++ CareerCloud.WebAPI/Controllers/CompanyLocationController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/company/v1")]
+    [ApiController]
+    public class CompanyLocationController : ControllerBase
+    {
+        private CompanyLocationLogic _logic;
+        
+
+        public CompanyLocationController()
+        {
+            var repo = new EFGenericRepository<CompanyLocationPoco>();
+            _logic = new CompanyLocationLogic(repo);
+        }
+
+        //Get on ID
+        [HttpGet]
+        [Route("location/{id}")]
+        public ActionResult GetCompanyLocation(Guid id)
+        {
+            CompanyLocationPoco poco = _logic.Get(id);
+            if (poco == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(poco);
+            }
+        }
+
+        //Get All
+        [HttpGet]
+        [Route("location")]
+        public ActionResult GetAllCompanyLocation()
+        {
+            List<CompanyLocationPoco> pocos = _logic.GetAll();
+            if (pocos == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(pocos);
+            }
+
+        }
+
+        //Post
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPost]
+        [Route("location")]
+        public ActionResult PostCompanyJobSkill([FromBody] CompanyLocationPoco[] companyLocationPocos)
+        {
+            _logic.Add(companyLocationPocos);
+            return Ok();
+        }
+
+        //Put
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPut]
+        [Route("location")]
+        public ActionResult PutCompanyLocation([FromBody] CompanyLocationPoco[] companyLocationPocos)
+        {
+            _logic.Update(companyLocationPocos);
+            return Ok();
+        }
+
+        //Delete
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpDelete]
+        [Route("location")]
+        public ActionResult DeleteCompanyJobSkill([FromBody] CompanyLocationPoco[] companyLocationPocos)
+        {
+            _logic.Delete(companyLocationPocos);
+            return Ok();
+        }
+
+    }
+}
