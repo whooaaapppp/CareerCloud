51c6f222e1e4092777ebe6d72fdc708d1b789220
whooaaapppp <jwarain@me.com>
2020-03-09 00:46:26 -0400

    +SecurityLoginsRole controller

---
+++ CareerCloud.WebAPI/Controllers/SecurityLoginsRoleController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/security/v1")]
+    [ApiController]
+    public class SecurityLoginsRoleController : ControllerBase
+    {
+        private SecurityLoginsRoleLogic _logic;
+        
+
+        public SecurityLoginsRoleController()
+        {
+            var repo = new EFGenericRepository<SecurityLoginsRolePoco>();
+            _logic = new SecurityLoginsRoleLogic(repo);
+        }
+
+        //Get on ID
+        [HttpGet]
+        [Route("loginsrole/{id}")]
+        public ActionResult GetSecurityLoginsRole(Guid id)
+        {
+            SecurityLoginsRolePoco poco = _logic.Get(id);
+            if (poco == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(poco);
+            }
+        }
+
+        //Get All
+        [HttpGet]
+        [Route("loginsrole")]
+        public ActionResult GetAllSecurityLoginsRole()
+        {
+            List<SecurityLoginsRolePoco> pocos = _logic.GetAll();
+            if (pocos == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(pocos);
+            }
+
+        }
+
+        //Post
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPost]
+        [Route("loginsrole")]
+        public ActionResult PostSecurityLoginsRole([FromBody] SecurityLoginsRolePoco[] securityLoginsRolePocos)
+        {
+            _logic.Add(securityLoginsRolePocos);
+            return Ok();
+        }
+
+        //Put
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPut]
+        [Route("loginsrole")]
+        public ActionResult PutSecurityLoginsRole([FromBody] SecurityLoginsRolePoco[] securityLoginsRolePocos)
+        {
+            _logic.Update(securityLoginsRolePocos);
+            return Ok();
+        }
+
+        //Delete
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpDelete]
+        [Route("loginsrole")]
+        public ActionResult DeleteSecurityLoginsRole([FromBody] SecurityLoginsRolePoco[] securityLoginsRolePocos)
+        {
+            _logic.Delete(securityLoginsRolePocos);
+            return Ok();
+        }
+
+    }
+}
