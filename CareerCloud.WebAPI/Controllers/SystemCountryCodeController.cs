80a7620b97e64013284a01deb00909f3e9682e05
whooaaapppp <jwarain@me.com>
2020-03-09 00:59:47 -0400

    +SystemLanguageCode and SystemCountryCode controllers and fixes

---
+++ CareerCloud.WebAPI/Controllers/SystemCountryCodeController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/system/v1")]
+    [ApiController]
+    public class SystemCountryCodeController : ControllerBase
+    {
+        private SystemCountryCodeLogic _logic;
+        
+
+        public SystemCountryCodeController()
+        {
+            var repo = new EFGenericRepository<SystemCountryCodePoco>();
+            _logic = new SystemCountryCodeLogic(repo);
+        }
+
+        //Get on ID
+        [HttpGet]
+        [Route("countrycode/{id}")]
+        public ActionResult GetSystemCountryCode(string id)
+        {
+            SystemCountryCodePoco poco = _logic.Get(id);
+            if (poco == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(poco);
+            }
+        }
+
+        //Get All
+        [HttpGet]
+        [Route("countrycode")]
+        public ActionResult GetAllSystemCountryCode()
+        {
+            List<SystemCountryCodePoco> pocos = _logic.GetAll();
+            if (pocos == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(pocos);
+            }
+
+        }
+
+        //Post
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPost]
+        [Route("countrycode")]
+        public ActionResult PostSystemCountryCode([FromBody] SystemCountryCodePoco[] systemCountryCodePocos)
+        {
+            _logic.Add(systemCountryCodePocos);
+            return Ok();
+        }
+
+        //Put
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPut]
+        [Route("countrycode")]
+        public ActionResult PutSystemCountryCode([FromBody] SystemCountryCodePoco[] systemCountryCodePocos)
+        {
+            _logic.Update(systemCountryCodePocos);
+            return Ok();
+        }
+
+        //Delete
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpDelete]
+        [Route("countrycode")]
+        public ActionResult DeleteSystemCountryCode([FromBody] SystemCountryCodePoco[] systemCountryCodePocos)
+        {
+            _logic.Delete(systemCountryCodePocos);
+            return Ok();
+        }
+
+    }
+}
---
+++ CareerCloud.WebAPI/Controllers/SystemLanguageCodeController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/system/v1")]
+    [ApiController]
+    public class SystemLanguageCodeController : ControllerBase
+    {
+        private SystemLanguageCodeLogic _logic;
+        
+
+        public SystemLanguageCodeController()
+        {
+            var repo = new EFGenericRepository<SystemLanguageCodePoco>();
+            _logic = new SystemLanguageCodeLogic(repo);
+        }
+
+        //Get on ID
+        [HttpGet]
+        [Route("languagecode/{id}")]
+        public ActionResult GetSystemLanguageCode(string id)
+        {
+            SystemLanguageCodePoco poco = _logic.Get(id);
+            if (poco == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(poco);
+            }
+        }
+
+        //Get All
+        [HttpGet]
+        [Route("languagecode")]
+        public ActionResult GetAllSystemLanguageCode()
+        {
+            List<SystemLanguageCodePoco> pocos = _logic.GetAll();
+            if (pocos == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(pocos);
+            }
+
+        }
+
+        //Post
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPost]
+        [Route("languagecode")]
+        public ActionResult PostSystemLanguageCode([FromBody] SystemLanguageCodePoco[] systemLanguageCodePocos)
+        {
+            _logic.Add(systemLanguageCodePocos);
+            return Ok();
+        }
+
+        //Put
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPut]
+        [Route("languagecode")]
+        public ActionResult PutSystemLanguageCode([FromBody] SystemLanguageCodePoco[] systemLanguageCodePocos)
+        {
+            _logic.Update(systemLanguageCodePocos);
+            return Ok();
+        }
+
+        //Delete
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpDelete]
+        [Route("languagecode")]
+        public ActionResult DeleteSystemLanguageCode([FromBody] SystemLanguageCodePoco[] systemLanguageCodePocos)
+        {
+            _logic.Delete(systemLanguageCodePocos);
+            return Ok();
+        }
+
+    }
+}
