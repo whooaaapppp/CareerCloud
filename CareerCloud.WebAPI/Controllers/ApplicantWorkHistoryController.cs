1baf02fa0550b2e1e9b656ad90c6d663f8b97d1a
whooaaapppp <jwarain@me.com>
2020-03-08 23:45:22 -0400

    +ApplicantWorkHistory controller

---
+++ CareerCloud.WebAPI/Controllers/ApplicantWorkHistoryController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.Pocos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareerCloud.WebAPI.Controllers
+{
+    [Route("api/careercloud/applicant/v1")]
+    [ApiController]
+    public class ApplicantWorkHistoryController : ControllerBase
+    {
+        private ApplicantWorkHistoryLogic _logic;
+        
+
+        public ApplicantWorkHistoryController()
+        {
+            var repo = new EFGenericRepository<ApplicantWorkHistoryPoco>();
+            _logic = new ApplicantWorkHistoryLogic(repo);
+
+
+        }
+
+        //Get on ID
+        [HttpGet]
+        [Route("workhistory/{id}")]
+        public ActionResult GetApplicantWorkHistory(Guid id)
+        {
+            ApplicantWorkHistoryPoco poco = _logic.Get(id);
+            if (poco == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(poco);
+            }
+        }
+
+        //Get All
+        [HttpGet]
+        [Route("workhistory")]
+        public ActionResult GetAllApplicantWorkHistory()
+        {
+            List<ApplicantWorkHistoryPoco> pocos = _logic.GetAll();
+            if (pocos == null)
+            {
+                //404
+                return NotFound();
+            }
+            else
+            {
+                //200
+                return Ok(pocos);
+            }
+
+        }
+
+        //Post
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPost]
+        [Route("workhistory")]
+        public ActionResult PostApplicantWorkHistory([FromBody] ApplicantWorkHistoryPoco[] applicantWorkHistoryPocos)
+        {
+            _logic.Add(applicantWorkHistoryPocos);
+            return Ok();
+        }
+
+        //Put
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpPut]
+        [Route("workhistory")]
+        public ActionResult PutApplicantWorkHistory([FromBody] ApplicantWorkHistoryPoco[] applicantWorkHistoryPocos)
+        {
+            _logic.Update(applicantWorkHistoryPocos);
+            return Ok();
+        }
+
+        //Delete
+        //To force Web API to read a simple type from the request body, add the[FromBody] attribute to the parameter
+        //https://docs.microsoft.com/en-us/aspnet/web-api/overview/formats-and-model-binding/parameter-binding-in-aspnet-web-api
+        [HttpDelete]
+        [Route("workhistory")]
+        public ActionResult DeleteApplicantWorkHistory([FromBody] ApplicantWorkHistoryPoco[] applicantWorkHistoryPocos)
+        {
+            _logic.Delete(applicantWorkHistoryPocos);
+            return Ok();
+        }
+
+
+
+
+    }
+}
