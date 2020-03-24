aa3d033cead012fc05f2d8cf7d345b6deae1c1a9
whooaaapppp <jwarain@me.com>
2020-03-24 16:19:07 -0400

    initial ApplicantEducation proto and service

---
+++ CareerCloud.gRPC/Services/ApplicantEducationService.cs
@@ -0,0 +1,48 @@
+﻿using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.gRPC.Protos;
+using CareerCloud.Pocos;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static CareerCloud.gRPC.Protos.ApplicantEducation;
+
+namespace CareerCloud.gRPC.Services
+{
+    public class ApplicantEducationService : ApplicantEducationBase
+    {
+        private readonly ApplicantEducationLogic _logic;
+        public ApplicantEducationService()
+        {
+            
+            _logic = new ApplicantEducationLogic(new EFGenericRepository<ApplicantEducationPoco>());
+
+        }
+
+        public override Task<ApplicantEducationPayload> ReadApplicantEducation(IdRequest request, ServerCallContext context)
+        {
+            ApplicantEducationPoco poco = _logic.Get(Guid.Parse(request.Id));
+
+            if (poco is null)
+            {
+                throw new ArgumentOutOfRangeException("Poco is null");
+
+            }
+            return new Task<ApplicantEducationPayload>(
+                () => new ApplicantEducationPayload()
+                {
+                    Id = poco.Id.ToString(),
+                    Applicant = poco.Applicant.ToString(),
+                    CertificateDiploma = poco.CertificateDiploma,
+                    CompletionDate = poco.CompletionDate is null ? null : Timestamp.FromDateTime((DateTime)poco.CompletionDate),
+                    CompletionPercent = poco.CompletionPercent is null ? 0 : (int)poco.CompletionPercent,
+                    Major = poco.Major,
+                    StartDate = poco.StartDate is null ? null : Timestamp.FromDateTime((DateTime)poco.StartDate),
+                }
+                ) ;
+        }
+    }
+}
