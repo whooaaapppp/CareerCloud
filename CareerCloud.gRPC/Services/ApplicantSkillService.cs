1d90622d441e74073376cd1f013d4b192fbb84b0
whooaaapppp <jwarain@me.com>
2020-03-24 21:10:02 -0400

    ApplicantSkil proto and service

---
+++ CareerCloud.gRPC/Services/ApplicantSkillService.cs
@@ -0,0 +1,52 @@
+﻿using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.gRPC.Protos;
+using CareerCloud.Pocos;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static CareerCloud.gRPC.Protos.ApplicantSkill;
+
+namespace CareerCloud.gRPC.Services
+{
+    public class ApplicantSkillService : ApplicantSkillBase
+    {
+        private readonly ApplicantSkillLogic _logic;
+
+        public ApplicantSkillService()
+        {
+            _logic = new ApplicantSkillLogic(new EFGenericRepository<ApplicantSkillPoco>());
+        }
+
+        public override Task<ApplicantSkillPayload> ReadApplicantSkill(IdRequestApplicantSkill request, ServerCallContext context)
+        {
+
+
+            ApplicantSkillPoco poco = _logic.Get(Guid.Parse(request.Id));
+
+            if (poco is null)
+            {
+                throw new ArgumentOutOfRangeException("Poco is null");
+
+            }
+            return new Task<ApplicantSkillPayload>(
+                () => new ApplicantSkillPayload()
+                {
+                    Id = poco.Id.ToString(),
+                    Applicant = poco.Applicant.ToString(),
+                    Skill = poco.Skill,
+                    SkillLevel = poco.SkillLevel,
+                    StartMonth = poco.StartMonth,
+                    StartYear = poco.StartYear,
+                    EndMonth = poco.EndMonth,
+                    EndYear = poco.EndYear
+                }
+                );
+        }
+
+
+    }
+}
