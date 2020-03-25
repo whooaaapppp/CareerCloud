7382b31be615d09a3048d19cfebe580d9ed8a65a
whooaaapppp <jwarain@me.com>
2020-03-25 19:47:20 -0400

    SecurityLoginsRole proto and service

---
+++ CareerCloud.gRPC/Services/SecurityLoginsRoleService.cs
@@ -0,0 +1,49 @@
+﻿using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.gRPC.Protos;
+using CareerCloud.Pocos;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CareerCloud.gRPC.Protos.SecurityLoginsRole;
+
+namespace CareerCloud.gRPC.Services
+{
+    public class SecurityLoginsRoleService : SecurityLoginsRoleBase
+    {
+        private readonly SecurityLoginsRoleLogic _logic;
+        
+
+        public SecurityLoginsRoleService()
+        {
+
+            _logic = new SecurityLoginsRoleLogic(new EFGenericRepository<SecurityLoginsRolePoco>());
+
+        }
+
+        public override Task<SecurityLoginsRolePayload> ReadSecurityLoginsRole(IdRequestSecurityLoginsRole request, ServerCallContext context)
+        {
+            SecurityLoginsRolePoco poco = _logic.Get(Guid.Parse(request.Id));
+
+            if (poco is null)
+            {
+                throw new ArgumentOutOfRangeException("Poco is null");
+
+            }
+            return new Task<SecurityLoginsRolePayload>(
+                () => new SecurityLoginsRolePayload()
+                {
+                    Id = poco.Id.ToString(),
+                    Login = poco.Login.ToString(),
+                    Role = poco.Role.ToString()
+                    
+        }
+                ) ;
+        }
+    }
+}
