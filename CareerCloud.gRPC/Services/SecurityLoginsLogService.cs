fca0db640a5c1cafcd13976ccacebbc4af6533a0
whooaaapppp <jwarain@me.com>
2020-03-25 19:30:17 -0400

    SecurityLoginsLog proto and service

---
+++ CareerCloud.gRPC/Services/SecurityLoginsLogService.cs
@@ -0,0 +1,50 @@
+﻿using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.gRPC.Protos;
+using CareerCloud.Pocos;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CareerCloud.gRPC.Protos.SecurityLoginsLog;
+
+namespace CareerCloud.gRPC.Services
+{
+    public class SecurityLoginsLogService : SecurityLoginsLogBase
+    {
+        private readonly SecurityLoginsLogLogic _logic;
+        
+
+        public SecurityLoginsLogService()
+        {
+
+            _logic = new SecurityLoginsLogLogic(new EFGenericRepository<SecurityLoginsLogPoco>());
+
+        }
+
+        public override Task<SecurityLoginsLogPayload> ReadSecurityLoginsLog(IdRequestSecurityLoginsLog request, ServerCallContext context)
+        {
+            SecurityLoginsLogPoco poco = _logic.Get(Guid.Parse(request.Id));
+
+            if (poco is null)
+            {
+                throw new ArgumentOutOfRangeException("Poco is null");
+
+            }
+            return new Task<SecurityLoginsLogPayload>(
+                () => new SecurityLoginsLogPayload()
+                {
+                    Id = poco.Id.ToString(),
+                    Login = poco.Login.ToString(),
+                    SourceIP = poco.SourceIP,
+                    LogonDate = Timestamp.FromDateTime(poco.LogonDate),
+                    IsSuccesful = poco.IsSuccesful
+        }
+                ) ;
+        }
+    }
+}
