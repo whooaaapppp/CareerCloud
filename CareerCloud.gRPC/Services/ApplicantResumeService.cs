9be0fa3c90ac0f4546da8b5c7f7a3e34cf0f7211
whooaaapppp <jwarain@me.com>
2020-03-24 20:53:31 -0400

    ApplicantResume proto and service

---
+++ CareerCloud.gRPC/Services/ApplicantResumeService.cs
@@ -0,0 +1,45 @@
+﻿using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.gRPC.Protos;
+using CareerCloud.Pocos;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static CareerCloud.gRPC.Protos.ApplicantResume;
+
+namespace CareerCloud.gRPC.Services
+{
+    public class ApplicantResumeService : ApplicantResumeBase
+    {
+        private readonly ApplicantResumeLogic _logic;
+
+        public ApplicantResumeService()
+        {
+            _logic = new ApplicantResumeLogic(new EFGenericRepository<ApplicantResumePoco>());
+        }
+
+        public override Task<ApplicantResumePayload> ReadApplicantResume(IdRequestApplicantResume request, ServerCallContext context)
+        {
+            ApplicantResumePoco poco = _logic.Get(Guid.Parse(request.Id));
+
+            if (poco is null)
+            {
+                throw new ArgumentOutOfRangeException("Poco is null");
+
+            }
+            return new Task<ApplicantResumePayload>(
+                () => new ApplicantResumePayload()
+                {
+                    Id = poco.Id.ToString(),
+                    Applicant = poco.Applicant.ToString(),
+                    Resume = poco.Resume,
+                    LastUpdated = poco.LastUpdated is null ? null : Timestamp.FromDateTime((DateTime)poco.LastUpdated),
+                    
+                }
+                );
+        }
+    }
+}
