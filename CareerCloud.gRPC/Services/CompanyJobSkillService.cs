30f70253896e1734fece5d896a419298697af847
whooaaapppp <jwarain@me.com>
2020-03-24 22:27:15 -0400

    CompanyJobSkill proto and service

---
+++ CareerCloud.gRPC/Services/CompanyJobSkillService.cs
@@ -0,0 +1,49 @@
+﻿using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.gRPC.Protos;
+using CareerCloud.Pocos;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static CareerCloud.gRPC.Protos.CompanyJobSkill;
+
+namespace CareerCloud.gRPC.Services
+{
+    public class CompanyJobSkillService : CompanyJobSkillBase
+    {
+        private readonly CompanyJobSkillLogic _logic;
+
+        public CompanyJobSkillService()
+        {
+
+            _logic = new CompanyJobSkillLogic(new EFGenericRepository<CompanyJobSkillPoco>());
+
+        }
+
+        public override Task<CompanyJobSkillPayload> ReadCompanyJobSkill(IdRequestCompanyJobSkill request, ServerCallContext context)
+        {
+
+
+            CompanyJobSkillPoco poco = _logic.Get(Guid.Parse(request.Id));
+
+            if (poco is null)
+            {
+                throw new ArgumentOutOfRangeException("Poco is null");
+
+            }
+            return new Task<CompanyJobSkillPayload>(
+                () => new CompanyJobSkillPayload()
+                {
+                    Id = poco.Id.ToString(),
+                    Job = poco.Job.ToString(),
+                    Skill = poco.Skill,
+                    SkillLevel = poco.SkillLevel,
+                    Importance = poco.Importance,
+                }
+                );
+        }
+    }
+}
