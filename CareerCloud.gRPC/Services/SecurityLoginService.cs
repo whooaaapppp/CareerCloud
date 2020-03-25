8db1fecd4139f57ff500892c4294d580082e8d80
whooaaapppp <jwarain@me.com>
2020-03-25 18:53:31 -0400

    SecurityLogin proto and service

---
+++ CareerCloud.gRPC/Services/SecurityLoginService.cs
@@ -0,0 +1,58 @@
+﻿using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.gRPC.Protos;
+using CareerCloud.Pocos;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CareerCloud.gRPC.Protos.SecurityLogin;
+
+namespace CareerCloud.gRPC.Services
+{
+    public class SecurityLoginService : SecurityLoginBase
+    {
+        private readonly SecurityLoginLogic _logic;
+        
+
+        public SecurityLoginService()
+        {
+
+            _logic = new SecurityLoginLogic(new EFGenericRepository<SecurityLoginPoco>());
+
+        }
+
+        public override Task<SecurityLoginPayload> ReadSecurityLogin(IdRequestSecurityLogin request, ServerCallContext context)
+        {
+            SecurityLoginPoco poco = _logic.Get(Guid.Parse(request.Id));
+
+            if (poco is null)
+            {
+                throw new ArgumentOutOfRangeException("Poco is null");
+
+            }
+            return new Task<SecurityLoginPayload>(
+                () => new SecurityLoginPayload()
+                {
+                    Id = poco.Id.ToString(),
+                    Login = poco.Login,
+                    Password = poco.Password,
+                    Created = Timestamp.FromDateTime(poco.Created),
+                    PasswordUpdate = poco.PasswordUpdate is null?null:Timestamp.FromDateTime((DateTime)poco.PasswordUpdate),
+                    AgreementAccepted = poco.AgreementAccepted is null?null:Timestamp.FromDateTime((DateTime)poco.AgreementAccepted),
+                    IsLocked = poco.IsLocked,
+                    IsInactive = poco.IsInactive,
+                    EmailAddress = poco.EmailAddress,
+                    PhoneNumber = poco.PhoneNumber,
+                    FullName = poco.FullName,
+                    ForceChangePassword = poco.ForceChangePassword,
+                    PreferredLanguage = poco.PrefferredLanguage
+        }
+                ) ;
+        }
+    }
+}
