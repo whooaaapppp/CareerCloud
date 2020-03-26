9b66b854839bb728d734a14d58203c4bbefb3238
whooaaapppp <jwarain@me.com>
2020-03-25 23:16:13 -0400

    CRUD ApplicantJobApplication

--- CareerCloud.gRPC/Services/ApplicantJobApplicationService.cs
+++ CareerCloud.gRPC/Services/ApplicantJobApplicationService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using CareerCloud.BusinessLogicLayer;
 using CareerCloud.EntityFrameworkDataAccess;
@@ -41,6 +42,69 @@ namespace CareerCloud.gRPC.Services
                 }
 
                 );
+        }
+
+        public override Task<Empty> CreateApplicantJobApplication(ApplicantJobApplicationPayload request, ServerCallContext context)
+        {
+            try
+            {
+                _logic.Add(new ApplicantJobApplicationPoco[] { new ApplicantJobApplicationPoco() {
+                    Id = new Guid(request.Id),
+                    Applicant = new Guid(request.Applicant),
+                    Job = new Guid(request.Job),
+                    ApplicationDate = Convert.ToDateTime(request.ApplicationDate)
+
+            } });
+            }
+            catch (AggregateException e)
+            {
+                IEnumerable<ValidationException> exceptions = e.InnerExceptions.Cast<ValidationException>();
+
+            }
+
+            return base.CreateApplicantJobApplication(request, context);
+        }
+
+        public override Task<Empty> UpdateApplicantJobApplication(ApplicantJobApplicationPayload request, ServerCallContext context)
+        {
+            try
+            {
+                _logic.Update(new ApplicantJobApplicationPoco[] { new ApplicantJobApplicationPoco() {
+                    Id = new Guid(request.Id),
+                    Applicant = new Guid(request.Applicant),
+                    Job = new Guid(request.Job),
+                    ApplicationDate = Convert.ToDateTime(request.ApplicationDate)
+
+            } });
+            }
+            catch (AggregateException e)
+            {
+                IEnumerable<ValidationException> exceptions = e.InnerExceptions.Cast<ValidationException>();
+
+            }
+
+            return base.UpdateApplicantJobApplication(request, context);
+        }
+
+        public override Task<Empty> DeleteApplicantJobApplication(ApplicantJobApplicationPayload request, ServerCallContext context)
+        {
+            try
+            {
+                _logic.Delete(new ApplicantJobApplicationPoco[] { new ApplicantJobApplicationPoco() {
+                    Id = new Guid(request.Id),
+                    Applicant = new Guid(request.Applicant),
+                    Job = new Guid(request.Job),
+                    ApplicationDate = Convert.ToDateTime(request.ApplicationDate)
+
+            } });
+            }
+            catch (AggregateException e)
+            {
+                IEnumerable<ValidationException> exceptions = e.InnerExceptions.Cast<ValidationException>();
+
+            }
+
+            return base.DeleteApplicantJobApplication(request, context);
         }
     }
 }
