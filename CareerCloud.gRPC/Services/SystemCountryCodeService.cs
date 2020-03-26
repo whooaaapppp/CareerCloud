6a87985d742ae1c0869973d1d93f4410d0dcf2c0
whooaaapppp <jwarain@me.com>
2020-03-25 20:24:17 -0400

    SystemCountryCode proto and service

---
+++ CareerCloud.gRPC/Services/SystemCountryCodeService.cs
@@ -0,0 +1,48 @@
+﻿using CareerCloud.BusinessLogicLayer;
+using CareerCloud.EntityFrameworkDataAccess;
+using CareerCloud.gRPC.Protos;
+using CareerCloud.Pocos;
+using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CareerCloud.gRPC.Protos.SystemCountryCode;
+
+namespace CareerCloud.gRPC.Services
+{
+    public class SystemCountryCodeService : SystemCountryCodeBase
+    {
+        private readonly SystemCountryCodeLogic _logic;
+        
+
+        public SystemCountryCodeService()
+        {
+
+            _logic = new SystemCountryCodeLogic(new EFGenericRepository<SystemCountryCodePoco>());
+
+        }
+
+        public override Task<SystemCountryCodePayload> ReadSystemCountryCode(IdRequestSystemCountryCode request, ServerCallContext context)
+        {
+            SystemCountryCodePoco poco = _logic.Get(request.Code);
+
+            if (poco is null)
+            {
+                throw new ArgumentOutOfRangeException("Poco is null");
+
+            }
+            return new Task<SystemCountryCodePayload>(
+                () => new SystemCountryCodePayload()
+                {
+                    Code = poco.Code,
+                    Name = poco.Name
+                    
+                }
+                ) ;
+        }
+    }
+}
